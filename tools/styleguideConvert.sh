45cd9f96604405939bfbe53eabfbe1750f7b9b25
rsoulliere <robert.soulliere@mohawkcollege.ca>
2011-10-17 14:00:35 -0400

    Update conversion process files.

--- tools/styleguideConvert.sh
+++ tools/styleguideConvert.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 
 #generate draft html
- xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/style_guide/html/ ~/Evergreen-DocBook/stylesheets/styleguide/evergreen_xhtml.xsl ~/Evergreen-DocBook/style_guide/root.xml
+ xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/style_guide/html/ ~/Evergreen_Documentation_Tools/stylesheets/styleguide/evergreen_xhtml.xsl ~/Evergreen_Documentation_Tools/style_guide/root.xml
 
 
 #Generate PDF via FO
- xsltproc --xinclude  --output ~/Evergreen-DocBook/style_guide/pdf/temp.fo ~/Evergreen-DocBook/stylesheets/styleguide/evergreen_fo.xsl ~/Evergreen-DocBook/style_guide/root.xml
+ xsltproc --xinclude  --output ~/Evergreen_Documentation_tools/style_guide/pdf/temp.fo ~/Evergreen_Documenttion_Tools/stylesheets/styleguide/evergreen_fo.xsl ~/Evergreen_Documentation_Tools/style_guide/root.xml
 
 # must run fop from same directory as root.xml
-cd ~/Evergreen-DocBook/style_guide/
+cd ~/Evergreen_Documentation_Tools/style_guide/
 
  ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/style_guide/pdf/style_guide.pdf 
 
