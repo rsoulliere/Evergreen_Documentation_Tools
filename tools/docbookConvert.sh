70e8a33b1c4d35c570904a7393f8adf03a16d3d5
rsoulliere <robert.soulliere@mohawkcollege.ca>
2011-11-24 15:03:46 -0500

    Add 2.1 to conversion to HTML and PDF.

--- tools/docbookConvert.sh
+++ tools/docbookConvert.sh
@@ -1,28 +1,42 @@
 #/bin/bash
 
 #generate 2.0 draft html
-xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/2.0/draft/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml-2.0.xsl ~/rel_2_0/root.xml
+xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/2.0/draft/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml-2.0.xsl ~/Evergreen_Documentation/rel_2_0/root.xml
 
 
 #Generate 2.0 PDF via FO
- xsltproc --xinclude  --output ~/rel_2_0/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/rel_2_0/root.xml
+ xsltproc --xinclude  --output ~/Evergreen_Documentation/rel_2_0/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen_Documentation/rel_2_0/root.xml
 
 # must run fop from same directory as root.xml
- cd ~/rel_2_0
+ cd ~/Evergreen_Documentation/rel_2_0
   ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/2.0/draft/pdf/Evergreen_Documentation.pdf 
  rm temp.fo
 
 #generate 1.6 draft html
-xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/1.6/draft/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml.xsl ~/rel_1_6/root.xml
+#xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/1.6/draft/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml.xsl ~/Evergreen_Documentation/rel_1_6/root.xml
 
 
 #Generate 1.6 PDF via FO
-xsltproc --xinclude  --output ~/rel_1_6/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/rel_1_6/root.xml
+#xsltproc --xinclude  --output ~/Evergreen_Documentation/rel_1_6/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen_Documentation/rel_1_6/root.xml
 
 # must run fop from same directory as root.xml
-cd ~/rel_1_6
+#cd ~/Evergreen_Documentation/rel_1_6
 
-~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/1.6/draft/pdf/Evergreen_Documentation.pdf 
+#~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/1.6/draft/pdf/Evergreen_Documentation.pdf 
 
 # remove temporary .fo file
-rm temp.fo
+#:rm temp.fo
+
+#generate 2.1 draft html
+xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/2.1/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml-2.1.xsl ~/Evergreen_Documentation/rel_2_1/root.xml
+
+
+#Generate 2.1 PDF via FO
+ xsltproc --xinclude  --output ~/Evergreen_Documentation/rel_2_1/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen_Documentation/rel_2_1/root.xml
+
+# must run fop from same directory as root.xml
+ cd ~/Evergreen_Documentation/rel_2_1
+  ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/2.0/draft/pdf/Evergreen_Documentation.pdf 
+ rm temp.fo
+
+
