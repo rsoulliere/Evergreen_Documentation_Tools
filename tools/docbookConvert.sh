02f72f381540b87aadaeae15948dc49997fb54f5
rsoulliere <robert.soulliere@mohawkcollege.ca>
2012-07-18 14:37:50 -0400

    Remove 2.0 from standard nightly processing.

--- tools/docbookConvert.sh
+++ tools/docbookConvert.sh
@@ -26,16 +26,16 @@ xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentat
  rm temp.fo
 
 #generate 2.0 draft html
-xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/2.0/draft/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml-2.0.xsl ~/Evergreen_Documentation/rel_2_0/root.xml
+# xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/2.0/draft/html/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml-2.0.xsl ~/Evergreen_Documentation/rel_2_0/root.xml
 
 
 #Generate 2.0 PDF via FO
- xsltproc --xinclude  --output ~/Evergreen_Documentation/rel_2_0/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen_Documentation/rel_2_0/root.xml
+# xsltproc --xinclude  --output ~/Evergreen_Documentation/rel_2_0/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen_Documentation/rel_2_0/root.xml
 
 # must run fop from same directory as root.xml
- cd ~/Evergreen_Documentation/rel_2_0
-  ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/2.0/draft/pdf/Evergreen_Documentation.pdf 
- rm temp.fo
+# cd ~/Evergreen_Documentation/rel_2_0
+#  ~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/2.0/draft/pdf/Evergreen_Documentation.pdf 
+# rm temp.fo
 
 
 
@@ -52,5 +52,5 @@ xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentat
 #~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/1.6/draft/pdf/Evergreen_Documentation.pdf 
 
 # remove temporary .fo file
-#:rm temp.fo
+#rm temp.fo
 
