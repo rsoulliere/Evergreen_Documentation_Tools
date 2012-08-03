e8363a036259af270fe14e4e3526ac274eea2abf
rsoulliere <robert.soulliere@mohawkcollege.ca>
2012-08-03 11:40:11 -0400

    Add 2.3 conversion tools.

---
+++ tools/2.3Convert.sh
@@ -0,0 +1,12 @@
+#generate 2.2 documentation
+xsltproc --xinclude --stringparam base.dir /openils/var/web/evergreen_documentation/2.3/ ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_xhtml-2.3.xsl ~/Evergreen_Documentation/rel_2_3/root.xml
+
+
+#Generate 2.2 PDF via FO
+xsltproc --xinclude  --output ~/Evergreen_Documentation/rel_2_3/pdf/temp.fo ~/Evergreen_Documentation_Tools/stylesheets/evergreen_docbook_files/evergreen_fo.xsl ~/Evergreen_Documentation/rel_2_3/root.xml
+
+# must run fop from same directory as root.xml
+cd ~/Evergreen_Documentation/rel_2_3
+~/doctools/fop/fop -fo pdf/temp.fo -pdf /openils/var/web/evergreen_documentation/2.3/Evergreen_Documentation.pdf 
+rm temp.fo
+
